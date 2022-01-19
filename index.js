a2e4f9a7a1f258671f670e3ab0e3fba68cc4e3f2
Mayuresh Chaudhari <84271416+MayureshNChaudhari@users.noreply.github.com>
2022-01-19 20:06:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+function one() {
+    document.getElementById("text").value = document.getElementById("text").value+document.getElementById("one").value
+  }
+function two() {
+    document.getElementById("text").value = document.getElementById("text").value+document.getElementById("two").value
+  }
+function three() {
+    document.getElementById("text").value = document.getElementById("text").value+document.getElementById("three").value
+  }
+function four() {
+    document.getElementById("text").value = document.getElementById("text").value+document.getElementById("four").value
+  }
+function five() {
+    document.getElementById("text").value = document.getElementById("text").value+document.getElementById("five").value
+  }
+function six() {
+    document.getElementById("text").value = document.getElementById("text").value+document.getElementById("six").value
+  }
+function seven() {
+    document.getElementById("text").value = document.getElementById("text").value+document.getElementById("seven").value
+  }
+function eight() {
+    document.getElementById("text").value = document.getElementById("text").value+document.getElementById("eight").value
+  }
+function nine() {
+    document.getElementById("text").value = document.getElementById("text").value+document.getElementById("nine").value
+  }
+function zero() {
+    document.getElementById("text").value = document.getElementById("text").value+document.getElementById("zero").value
+  }
+function mul() {
+    document.getElementById("text").value = document.getElementById("text").value+document.getElementById("mul").value
+  }
+function div() {
+    document.getElementById("text").value = document.getElementById("text").value+document.getElementById("div").value
+  }
+function minus() {
+    document.getElementById("text").value = document.getElementById("text").value+document.getElementById("minus").value
+  }
+function plus() {
+    document.getElementById("text").value = document.getElementById("text").value+document.getElementById("plus").value
+  }
+function brac() {
+    document.getElementById("text").value = document.getElementById("text").value+document.getElementById("brac").value
+  }
+function br() {
+    document.getElementById("text").value = document.getElementById("text").value+document.getElementById("br").value
+  }
+function per() {
+    document.getElementById("text").value = document.getElementById("text").value+document.getElementById("per").value
+  }
+function dot() {
+    document.getElementById("text").value = document.getElementById("text").value+document.getElementById("dot").value
+  }
+function AC() {
+document.location.reload()  }
+function equal() {
+  try{
+ document.getElementById("text").value= eval(document.getElementById("text").value)}
+ catch {
+   document.getElementById("text").value="ERROR"
+ }
+}
